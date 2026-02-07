f1b97971533587052957135cb75ff481c9ec4052
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2026-02-07 17:55:28 +0100

    update

---
+++ hardware/ELPM-S3LW/firmware/PlatformIO/Powerlatch-EdgeDetector/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <esp_adc_cal.h>
+#include <esp32-hal-adc.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN        48
+#define NUMPIXELS   1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define AUTO_TURN_OFF 14
+#define BattLVL 7
+#define PLATCH_IN 6
+
+void setup() {
+  Serial.begin(115200);
+  // Power latch setup
+  pinMode(AUTO_TURN_OFF, OUTPUT);
+  digitalWrite(AUTO_TURN_OFF, HIGH);
+  Serial.println("system started");
+
+  pixels.begin(); 
+  pixels.setPixelColor(0, pixels.Color(0, 50, 0));
+  pixels.show(); 
+}
+
+void loop() {
+  delay(5000);
+  digitalWrite(AUTO_TURN_OFF, LOW);
+}
+
